f6bb27f059f44d810a078851fe6697bf6d03b6c6
ni3aj <nirajnpatil83@gmail.com>
2026-08-18 13:09:24 +0530

    deleted sdui

--- src/components/sdui/SpacerWidget.js
+++
@@ -1,5 +0,0 @@
-import { View } from "react-native";
-
-export default function SpacerWidget({ height = 20 }) {
-  return <View style={{ height }} />;
-}
--- src/components/sdui/WidgetRegistry.js
+++
@@ -1,9 +0,0 @@
-import GradientCardWidget from "./GradientCardWidget";
-import SectionHeaderWidget from "./SectionHeaderWidget";
-import SpacerWidget from "./SpacerWidget"; // 📌 Add this
-
-export const WidgetRegistry = {
-  SectionHeaderWidget: SectionHeaderWidget,
-  GradientCardWidget: GradientCardWidget,
-  SpacerWidget: SpacerWidget, // 📌 Add this
-};
